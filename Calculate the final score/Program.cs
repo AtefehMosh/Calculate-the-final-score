536c1f0694461f6e94ec6efb3f91e262ed8f6338
AtefehMosh <elina.ee1.8532@gmail.com>
2023-04-16 08:01:13 +0430

    Add project files.

---
+++ Calculate the final score/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Calculate_the_final_score
+{
+    class Program
+
+    { 
+        static void Main(string[] args)
+       {
+        Console.Write("Please Enter midterm Mark.. ");
+        double midtermMark = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Please Enter final mark.. ");
+        double finalMark = Convert.ToDouble(Console.ReadLine());
+            double show = Clac(midtermMark, finalMark);
+         
+            if (show>= 10)
+                Console.WriteLine("You pass the Exam and your grade is: "+show);
+        } 
+        
+        private static double Clac(double x,double y)
+        {
+            double result = (0.35 * x) + (0.65 * y);
+            return result;
+                
+        }
+    
+    }
+}
